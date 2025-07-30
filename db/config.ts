456661f4f9b896d8ebb920397527827b78dd74ce
Felipe Navas <felipenavas.itec@gmail.com>
2025-07-30 19:34:54 -0300

    Add bcrypt, uuid, and tables with seed

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,26 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db'
 
-// https://astro.build/db/config
+const User = defineTable({
+  columns: {
+    id: column.text({ primary: true, unique: true }),
+    name: column.text(),
+    email: column.text({ unique: true }),
+    password: column.text(),
+    createdAt: column.date({ default: new Date() }),
+    role: column.text({ references: () => Role.columns.id }),
+  },
+})
+
+const Role = defineTable({
+  columns: {
+    id: column.text({ primary: true, unique: true }),
+    name: column.text(),
+  },
+})
+
 export default defineDb({
-  tables: {}
-});
+  tables: {
+    User,
+    Role,
+  },
+})
